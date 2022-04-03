d25717f6f83f3bcd05873ff3e41220a20742ee46
Gianluca <gianluca.profile@gmail.com>
2022-04-03 12:53:57 +0200

    new Files

---
+++ source/BigStore/BigStoreCore_CustomerExtended/EFBusinessLayer_CustomerExtended.cs
@@ -0,0 +1,21 @@
+﻿using BigStoreCore.Interfaces;
+using BigStoreCore.Logic;
+using BigStoreCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigStoreCore_CustomerExtended
+{
+    public class EFBusinessLayer_CustomerExtended : EFBusinessLayer
+    {
+        public BigStoreContext _context;
+        public EFBusinessLayer_CustomerExtended(BigStoreContext context): base(context)
+        {
+        }
+
+        public override async Task<List<Product>> GetProducts()
+        {
+            List<Product> prod_ext = await base.GetProducts();
+            return prod_ext.Where(p => p.ProductId%2 ==0).ToList();
+        }
+    }
+}
