ee08a77a99e1f783857d2826b826bfcd5dbaa0a4
Gianluca <gianluca.profile@gmail.com>
2022-04-19 11:36:07 +0200

    nSwagClient + Consuming client test

--- source/BigStoreWeb/src/app/app.component.ts
+++ source/BigStoreWeb/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BigStoreClientComponent, Client } from './big-store-client/big-store-client.component';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'BigStoreWeb';
+    service!: Client; 
+
+    constructor(private http:HttpClient){
+
+    }
+   ngOnInit() {
+   // this.service = new BigStoreClientComponent();
+
+     this.service = new Client("https://localhost:7008");
+     console.log('Products: ');
+     this.service.products().then(
+      (val) => console.log( val),
+      (err) => console.log(err)
+    );
+
+    console.log('Categories: ');
+    this.service.categories().then(
+      (val) => console.log( val),
+      (err) => console.log(err)
+    );
+    }
 }
--- source/BigStoreWeb/src/app/app.module.ts
+++ source/BigStoreWeb/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigStoreClientComponent } from './big-store-client/big-store-client.component';
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
+    BigStoreClientComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
