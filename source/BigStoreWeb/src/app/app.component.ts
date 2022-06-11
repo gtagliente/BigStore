d9c9aa5a6e24c48a88daa7d2bd4dcfe094428436
Gianluca <gianluca.profile@gmail.com>
2022-06-11 17:11:57 +0200

    Angular - file di config.json iniettato nel provider globale. - estensione comportamento di routing per supportare lo sliding su sitch di tipo nei componenti MenuPanelComponent

--- source/BigStoreWeb/src/app/app.component.ts
+++ source/BigStoreWeb/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BigStoreClientComponent, Client } from './big-store-client/big-store-client.component';
 import {HttpClient} from "@angular/common/http";
+import { BigStoreClientService,Client } from './big-store-client.service';
 
 
 @Component({
@@ -10,23 +10,24 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent implements OnInit{
   title = 'BigStoreWeb';
-    service!: Client; 
 
-    constructor(private http:HttpClient){
+    constructor(private clientService:Client){
 
     }
    ngOnInit() {
    // this.service = new BigStoreClientComponent();
 
-     this.service = new Client("https://localhost:7008");
-     console.log('Products: ');
-     this.service.products().then(
+     //this.service = new Client("https://localhost:7008");
+     console.log('Client: '+this.clientService.baseUrl);
+     
+     console.log('Products  : ');
+     this.clientService.products().then(
       (val) => console.log( val),
       (err) => console.log(err)
     );
 
     console.log('Categories: ');
-    this.service.categories().then(
+    this.clientService.categories().then(
       (val) => console.log( val),
       (err) => console.log(err)
     );
--- source/BigStoreWeb/src/app/app.module.ts
+++ source/BigStoreWeb/src/app/app.module.ts
@@ -1,24 +1,61 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER } from '@angular/core';
+import { AppConfigService }       from './Config/app.config';
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { BigStoreClientComponent } from './big-store-client/big-store-client.component';
-import {HttpClientModule} from '@angular/common/http';
+import { BigStoreClientService, Client } from './big-store-client.service';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import { MenuComponent } from './Menu/menu.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuPanelComponent } from './Menu/menu-panel/menu-panel.component';
+import { MenuItemComponent } from './Menu/menu-item/menu-item.component';
+import { AppRoutingModule } from './Routing/app-routing.module';
 
+
+
+
+// const appRoutes: Routes = [
+//     { path: '', component: MenuPanelComponent},
+//     { path: 'MenuPanel/:type', component: MenuPanelComponent}
+//   ];
 
 
 @NgModule({
   declarations: [
-    BigStoreClientComponent,
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuPanelComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
+    //RouterModule.forRoot(appRoutes),
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [AppConfigService],
+      useFactory: (appConfigService: AppConfigService) => {
+        return () => {
+          //Make sure to return a promise!
+          return appConfigService.loadAppConfig();
+        };
+      }
+    },
+    {
+      provide: Client,
+      deps: [AppConfigService],
+      useFactory: (appConfigService: AppConfigService) => {
+          return new Client(appConfigService.apiBaseUrl);
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
